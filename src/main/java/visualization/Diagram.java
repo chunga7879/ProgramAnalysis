c1c0a93b24428e03eca635727b1441e9dc011f10
katherine.liang <katherine.liang.0@gmail.com>
2022-11-24 21:43:54 -0800

    Renamed methods.

--- src/main/java/visualization/Diagram.java
+++ src/main/java/visualization/Diagram.java
@@ -11,12 +11,12 @@ public class Diagram {
         diagramString = new StringBuilder();
     }
 
-    public void startDiagram() {
+    public void addStartDiagramNode() {
         diagramString.append("@startuml\n");
         diagramString.append("start\n");
     }
 
-    public void endDiagram() {
+    public void addEndDiagramNode() {
         diagramString.append("stop\n");
         diagramString.append("@enduml");
     }
@@ -42,7 +42,7 @@ public class Diagram {
      * Adds a node to the diagram for a if conditional's condition
      * @param node Node that represents the diagram node. The node's statement should just be the conditional.
      */
-    public void addIfThenNode(DiagramNode node) {
+    public void addIfThenStartNode(DiagramNode node) {
         String conditional = "if (" + node.statement() + ") then (true)\n";
 //        DiagramNode conditionalNode = new DiagramNode(conditional, node.error(), node.errorDescription());
 //        addNode(conditionalNode);
@@ -53,7 +53,7 @@ public class Diagram {
         diagramString.append("else (false)\n");
     }
 
-    public void addEndIfNode() {
+    public void addIfEndNode() {
         diagramString.append("endif\n");
     }
 
@@ -61,14 +61,14 @@ public class Diagram {
      * Adds a node to the diagram for a while loop's condition
      * @param node Node that represents the diagram node. The statement of the node should just be the conditional of the while loop.
      */
-    public void addWhileLoopConditionalNode(DiagramNode node) {
+    public void addWhileConditionalStartNode(DiagramNode node) {
         String conditional = "while (" + node.statement() + ")\n";
 //        DiagramNode conditionalNode = new DiagramNode(conditional, node.error(), node.errorDescription());
 //        addNode(conditionalNode);
         diagramString.append(conditional);
     }
 
-    public void addEndWhileLoopNode() {
+    public void addWhileEndNode() {
         diagramString.append("endwhile\n");
     }
 
