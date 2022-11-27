7f8eb6a7e3ef8edc430ffaa694ea4a7d8832298b
emtsn <65764699+emtsn@users.noreply.github.com>
2022-11-26 17:21:12 -0800

    Restrict values, set isDefinite for array errors

--- src/test/java/analysis/visitor/ParameterInitializationTest.java
+++ src/test/java/analysis/visitor/ParameterInitializationTest.java
@@ -11,13 +11,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static analysis.visitor.VisitorTestUtils.compile;
-import static analysis.visitor.VisitorTestUtils.getParameter;
+import static analysis.visitor.VisitorTestUtils.*;
 
 public class ParameterInitializationTest {
     @BeforeEach
     public void runBefore() {
-        AnalysisLogger.setLog(true);
+        AnalysisLogger.setLog(false);
     }
 
     @Test
