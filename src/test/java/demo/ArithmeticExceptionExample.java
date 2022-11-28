35a37105873a1be6cd95a8017c0bc648e87df7c6
emtsn <65764699+emtsn@users.noreply.github.com>
2022-11-28 13:10:32 -0800

    Remove

--- src/test/java/demo/ArithmeticExceptionExample.java
+++ src/test/java/demo/ArithmeticExceptionExample.java
@@ -4,11 +4,6 @@ public class ArithmeticExceptionExample {
 
     // throws ArithmeticException if y is 0
     int divisionByZero(int x, int y) {
-        switch (x) {
-            default -> {
-                y = 2;
-            }
-        }
         return x / y;
     }
 }
